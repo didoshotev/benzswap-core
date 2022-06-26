da9b44336c72e6c8d29f30bee6c2ea03f1ae2399
didoshotev <didoshotev@gmail.com>
2022-06-26 20:13:10 +0300

    feat: add types/typechain

--- scripts/01-deploy-raffle.ts
+++ scripts/01-deploy-raffle.ts
@@ -1,3 +1,5 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Raffle__factory, VRFCoordinatorV2Mock__factory } from "../typechain";
 const { network, ethers } = require("hardhat");
 const { networkConfig, developmentChains } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
@@ -10,24 +12,35 @@ const GAS_PRICE_LINK = 1e9 // calculated value based on the gas price of the cha
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
 const main = async function () {
-    // const { deploy, log } = deployments
-    const [deployer, player1] = await ethers.getSigners()
+    const [deployer, player1]: SignerWithAddress[] = await ethers.getSigners()
 
     const chainId = network.config.chainId;
     let vrfCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
-        // const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
-        const VRFCoordinatorV2Mock = await ethers.getContractFactory("VRFCoordinatorV2Mock")
-        const vrfCoordinatorV2Mock = await VRFCoordinatorV2Mock.deploy(BASE_FEE, GAS_PRICE_LINK)
+        const vrfCoordinatorV2Mock = await new VRFCoordinatorV2Mock__factory(deployer).deploy(
+            BASE_FEE,
+            GAS_PRICE_LINK
+        )
+
+        await vrfCoordinatorV2Mock.deployed()
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
 
+        // const txResponse2 = await (await vrfCoordinatorV2Mock.createSubscription()).wait
         const txResponse = await vrfCoordinatorV2Mock.createSubscription()
         const txReceipt = await txResponse.wait();
-        subscriptionId = txReceipt.events[0].args.subId
+        if (!txReceipt.events) {
+            console.log('no such events: ');
+            return;
+        }
+        subscriptionId = txReceipt.events[0].args?.subId
         // Fund the sub
-        const txFundResponse = await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
-        const txFundReceipt = await txFundResponse.wait()
+        const txFundReceipt = await (await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)).wait()
+        if (!(txFundReceipt.events && txFundReceipt.events[0].args)) {
+            console.log('no such events in vrfCoordinatorV2Mock.fundSubscription');
+            return;
+        }
+
         console.log('------------------------Successfully funded------------------------');
         console.log("OLD balance: ", ethers.utils.formatEther(txFundReceipt.events[0].args[1]));
         console.log("NEW balance: ", ethers.utils.formatEther(txFundReceipt.events[0].args[2]));
@@ -50,14 +63,15 @@ const main = async function () {
         subscriptionId,
         callbackGasLimit,
         interval
-    ]
+    ] as const
 
-    const Raffle = await ethers.getContractFactory("Raffle")
-    const raffle = await Raffle.deploy(...args)
+    const raffle = await new Raffle__factory(deployer).deploy(
+        ...args
+    )
 
-     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-         await verify(raffle.address, args)
-     }
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(raffle.address, args)
+    }
 }
 
 main()
