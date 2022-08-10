7164597a92461e2efef3715001ff3fb7ebfb05bf
didoshotev <didoshotev@gmail.com>
2022-08-10 22:20:12 +0300

    feat: uniswap flash swap

--- contracts/defi/UniswapV2.sol
+++ contracts/defi/UniswapV2.sol
@@ -6,7 +6,7 @@ import "../interfaces/IUniswap.sol";
 import "../interfaces/IUniswapV2Callee.sol";
 import "hardhat/console.sol";
 
-contract UniswapV2 {
+contract UniswapV2 is IUniswapV2Callee {
     address private constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address private constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
@@ -100,13 +100,49 @@ contract UniswapV2 {
         emit Log("amountB", amountB);
     }
 
-    // function uniswapV2Call(
-    //     address _sender,
-    //     uint256 _amount0,
-    //     uint256 _amount1,
-    //     bytes calldata _data
-    // ) external override {
-    //     address token0 = IUniswapV2Pair(msg.sender).token0();
-    //     address token1 = IUniswapV2Pair(msg.sender).token1();
-    // }
+
+    // FLASH SWAP
+    function testFlashSwap(address _tokenBorrow, uint256 _amount) external { 
+        address pair = IUniswapV2Factory(FACTORY).getPair(_tokenBorrow, WETH);
+        require(pair != address(0), "Invalid pair");
+
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+        uint256 amount0Out = _tokenBorrow == token0 ? _amount : 0;
+        uint256 amount1Out = _tokenBorrow == token1 ? _amount : 0;
+
+        // pass data to trigger uniswapV2Call
+        bytes memory data = abi.encode(_tokenBorrow, _amount);
+        IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), data);
+
+    }
+
+    function uniswapV2Call(
+        address _sender,
+        uint256 _amount0,
+        uint256 _amount1,
+        bytes calldata _data
+    ) external override {
+        // check the function is called by the pair contract => msg.sender == pair
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        address pair = IUniswapV2Factory(FACTORY).getPair(token0, token1);
+        require(msg.sender == pair, "Not called by the pair");
+        require(_sender == address(this), "!sender"); // sender must be this contract
+
+        (address tokenBorrow, uint256 amount) = abi.decode(_data, (address, uint)); 
+
+        // about 0.3% fee
+        uint256 fee = ((amount * 3) / 997) + 1;
+        uint256 amountToRepay = amount + fee;
+
+        console.log("----------CONSOLE LOGS----------");
+        console.log("amount: ", amount);
+        console.log("amount0: ", _amount0);
+        console.log("amount1: ", _amount1);
+        console.log("fee: ", fee);
+        console.log("amount to repay: ", amountToRepay);
+        // emit Log("amount to repay: ", amountToRepay);
+        IERC20(tokenBorrow).transfer(pair, amountToRepay);
+    }
 }
--- test/unit/defi/uniswap-flash-swap.spec.ts
+++ test/unit/defi/uniswap-flash-swap.spec.ts
@@ -6,12 +6,13 @@ import { getNetworkConfigItem } from "../../../utils/helper"
 import IERC20_ABI from '../../../abi/IERC20_abi.json'
 import DAI_ABI from '../../../abi/DAI_abi.json'
 import { Receipt } from "hardhat-deploy/types"
+import { expect } from "chai"
 
 if (!developmentChains.includes(network.name)) {
     describe.skip
 }
 
-describe.skip("Uniswap Swap Tokens", function () {
+describe("Uniswap Swap Tokens", function () {
     let UniswapV2: UniswapV2;
     let weth_whale: SignerWithAddress;
     let deployer: SignerWithAddress;
@@ -22,8 +23,11 @@ describe.skip("Uniswap Swap Tokens", function () {
     let DAI_Instance: IERC20
     let WETH_Instance: IERC20
 
-    const TOKEN_A_AMOUNT = ethers.utils.parseEther("300")
-    const TOKEN_B_AMOUNT = ethers.utils.parseEther("300")
+    const TOKEN_A_AMOUNT = ethers.utils.parseEther("3000")
+    const TOKEN_B_AMOUNT = ethers.utils.parseEther("3000")
+
+    const FUND_AMOUNT = ethers.utils.parseEther("2000")
+    const BORROW_AMOUNT = ethers.utils.parseEther("1000")
 
     before(async function () {
         [deployer, user1] = await ethers.getSigners()
@@ -31,6 +35,7 @@ describe.skip("Uniswap Swap Tokens", function () {
         WETH_Instance = await ethers.getContractAt<IERC20>(IERC20_ABI, WETH, deployer);
         DAI_Instance = await ethers.getContractAt<IERC20>(DAI_ABI, DAI, deployer);
         UniswapV2 = await ethers.getContract("UniswapV2", deployer);
+
 
         if (!WETH_WHALE) {
             console.log('You must have WBTC_WHALE set in your env variables');
@@ -49,7 +54,7 @@ describe.skip("Uniswap Swap Tokens", function () {
 
         await WETH_Instance.connect(weth_whale).transfer(user1.address, TOKEN_A_AMOUNT)
         await DAI_Instance.connect(weth_whale).transfer(user1.address, TOKEN_B_AMOUNT)
-        
+
         await WETH_Instance.connect(user1).approve(UniswapV2.address, TOKEN_A_AMOUNT)
         await DAI_Instance.connect(user1).approve(UniswapV2.address, TOKEN_B_AMOUNT)
 
@@ -58,24 +63,17 @@ describe.skip("Uniswap Swap Tokens", function () {
 
         console.log('userDai: ', userDai);
         console.log('userWeth: ', userWeth);
+    
+        await DAI_Instance.connect(weth_whale).transfer(UniswapV2.address, FUND_AMOUNT)
+        const contractDaiBalance = ethers.utils.formatEther(await DAI_Instance.balanceOf(UniswapV2.address))
+        console.log('contractDaiBalance: ', contractDaiBalance);
     })
 
-    it("Should add liquidity and remove liquidity", async function () {
-        const wethBalanceBefore = ethers.utils.formatEther(await WETH_Instance.balanceOf(weth_whale.address))
-        const daiBalanceBefore = ethers.utils.formatEther(await DAI_Instance.balanceOf(weth_whale.address))
-
-        const value = ethers.utils.parseEther("300")
-        const addLiquidityTx = await UniswapV2.connect(user1).addLiquidity(
-            WETH_Instance.address,
-            DAI_Instance.address,
-            value,
-            value
-        )
-        const addLiquidityReceipt: Receipt = await addLiquidityTx.wait()
-        
-        const removeLiquidityTx = await UniswapV2.connect(user1).removeLiquidity(
-            WETH_Instance.address,
-            DAI_Instance.address
-        )        
+    it("flash swap", async function () {
+        console.log('9199');
+        const swapTx = await UniswapV2.testFlashSwap(DAI_Instance.address, BORROW_AMOUNT)
+        const swapReceipt = await swapTx.wait()
+        console.log('swapReceipt: ', swapReceipt);
+        expect(1).equal(1)
     })
 })
